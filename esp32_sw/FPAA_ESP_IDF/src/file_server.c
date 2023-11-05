fb96c400861ff0a26bdd507b1c1da80513ed17f6
James Newton <jamesnewton@massmind.org>
2023-11-05 08:42:07 -0800

    Integrate web socket service into http server

--- esp32_sw/FPAA_ESP_IDF/src/file_server.c
+++ esp32_sw/FPAA_ESP_IDF/src/file_server.c
@@ -25,7 +25,7 @@
 #include "esp_vfs.h"
 #include "esp_spiffs.h"
 #include "esp_http_server.h"
-
+#include "protocol_examples_common.h"
 #include "fpaa_spi.h"
 #include "file_server.h"
 
@@ -462,8 +462,8 @@ static esp_err_t fpaa_program_handler(httpd_req_t *req)
 {
 
     char filepath[FILE_PATH_MAX];
-    const char *filename = get_path_from_uri(filepath, ((struct file_server_data *)req->user_ctx)->base_path,
-                                             req->uri + sizeof("/fpaa_program") - 1, sizeof(filepath));
+//    const char *filename = get_path_from_uri(filepath, ((struct file_server_data *)req->user_ctx)->base_path,
+//                                             req->uri + sizeof("/fpaa_program") - 1, sizeof(filepath));
     ESP_LOGI(TAG, "Attempting to program FPAA with file: '%s'", filepath);
 
     program_fpaa(((struct file_server_data *)req->user_ctx)->spi_dev, filepath);
@@ -480,6 +480,113 @@ static esp_err_t fpaa_program_handler(httpd_req_t *req)
 static esp_err_t play_wav_handler(httpd_req_t *req)
 {
     return ESP_OK;
+}
+
+/* Web socket service stuff */
+
+
+/* A simple example that demonstrates using websocket echo server
+ */
+static const char *WTAG = "ws_echo_server";
+
+/*
+ * Structure holding server handle
+ * and internal socket fd in order
+ * to use out of request send
+ */
+struct async_resp_arg {
+    httpd_handle_t hd;
+    int fd;
+};
+
+/*
+ * async send function, which we put into the httpd work queue
+ */
+static void ws_async_send(void *arg)
+{
+    static const char * data = "Async data";
+    struct async_resp_arg *resp_arg = arg;
+    httpd_handle_t hd = resp_arg->hd;
+    int fd = resp_arg->fd;
+    httpd_ws_frame_t ws_pkt;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.payload = (uint8_t*)data;
+    ws_pkt.len = strlen(data);
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+
+    httpd_ws_send_frame_async(hd, fd, &ws_pkt);
+    free(resp_arg);
+}
+
+//sample special response, triggered by some keyword in the recieved message
+static esp_err_t trigger_async_send(httpd_handle_t handle, httpd_req_t *req)
+{
+    struct async_resp_arg *resp_arg = malloc(sizeof(struct async_resp_arg));
+    if (resp_arg == NULL) {
+        return ESP_ERR_NO_MEM;
+    }
+    resp_arg->hd = req->handle;
+    resp_arg->fd = httpd_req_to_sockfd(req);
+    esp_err_t ret = httpd_queue_work(handle, ws_async_send, resp_arg);
+    if (ret != ESP_OK) {
+        free(resp_arg);
+    }
+    return ret;
+}
+
+
+/*
+ * This handler echos back the received ws data
+ * and triggers an async send if certain message received
+ */
+static esp_err_t echo_handler(httpd_req_t *req)
+{
+    if (req->method == HTTP_GET) {
+        ESP_LOGI(WTAG, "Handshake done, the new connection was opened");
+        return ESP_OK;
+    }
+    httpd_ws_frame_t ws_pkt;
+    uint8_t *buf = NULL;
+    memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+    ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+    /* Set max_len = 0 to get the frame len */
+    esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
+    if (ret != ESP_OK) {
+        ESP_LOGE(WTAG, "httpd_ws_recv_frame failed to get frame len with %d", ret);
+        return ret;
+    }
+    ESP_LOGI(WTAG, "frame len is %d", ws_pkt.len);
+    if (ws_pkt.len) {
+        /* ws_pkt.len + 1 is for NULL termination as we are expecting a string */
+        buf = calloc(1, ws_pkt.len + 1);
+        if (buf == NULL) {
+            ESP_LOGE(WTAG, "Failed to calloc memory for buf");
+            return ESP_ERR_NO_MEM;
+        }
+        ws_pkt.payload = buf;
+        /* Set max_len = ws_pkt.len to get the frame payload */
+        ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
+        if (ret != ESP_OK) {
+            ESP_LOGE(WTAG, "httpd_ws_recv_frame failed with %d", ret);
+            free(buf);
+            return ret;
+        }
+        ESP_LOGI(WTAG, "Got packet with message: %s", ws_pkt.payload);
+    }
+    ESP_LOGI(WTAG, "Packet type: %d", ws_pkt.type);
+    if (ws_pkt.type == HTTPD_WS_TYPE_TEXT &&
+        strcmp((char*)ws_pkt.payload,"Trigger async") == 0) {
+        free(buf);
+        return trigger_async_send(req->handle, req);
+    }
+
+    //just echo it back out. DOTO: expand this to echo to all connections
+    ret = httpd_ws_send_frame(req, &ws_pkt);
+    if (ret != ESP_OK) {
+        ESP_LOGE(WTAG, "httpd_ws_send_frame failed with %d", ret);
+    }
+    free(buf);
+    return ret;
 }
 
 
@@ -517,6 +624,15 @@ esp_err_t start_file_server(const char *base_path, spi_device_handle_t spi_dev)
         ESP_LOGE(TAG, "Failed to start file server!");
         return ESP_FAIL;
     }
+
+    static const httpd_uri_t ws_uri = {
+            .uri        = "/ws",
+            .method     = HTTP_GET,
+            .handler    = echo_handler,
+            .user_ctx   = NULL,
+            .is_websocket = true
+    };
+    httpd_register_uri_handler(server, &ws_uri);
 
     /* URI handler for getting uploaded files */
     httpd_uri_t file_download = {
